fc1c643f82619f0e48da56fc7deba61feb60055c
Sebastian Männling <sebastian.maennling@exasol.com>
2026-05-21 12:46:56 +0200

    module path changed

--- hooks.js
+++ hooks.js
@@ -310,8 +310,8 @@ function isValidEmail(email) {
 }
 
 // n8n module paths (specific to the Docker image)
-const N8N_DI_PATH = '/usr/local/lib/node_modules/n8n/node_modules/@n8n/di';
-const N8N_JWT_SERVICE_PATH = '/usr/local/lib/node_modules/n8n/dist/services/jwt.service.js';
+const N8N_DI_PATH = '/usr/lib/node_modules/n8n/node_modules/@n8n/di';
+const N8N_JWT_SERVICE_PATH = '/usr/lib/node_modules/n8n/dist/services/jwt.service.js';
 
 // Export the hooks
 module.exports = {
